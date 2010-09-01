d17a92493aa813b26375468f7857fdaa55d036bc
Tom Carden <tom@tom-carden.co.uk>
2010-09-01 05:21:18 -0700

    multitouch with rotation etc.

--- src/modestmapsciApp.cpp
+++ src/modestmapsciApp.cpp
@@ -9,21 +9,28 @@ using namespace std;
 class modestmapsciApp : public AppBasic {
  public:
 
+	void prepareSettings(Settings *settings);
 	void setup();
 	void touchesBegan( TouchEvent event );
 	void touchesMoved( TouchEvent event );
 	void touchesEnded( TouchEvent event );
 	void keyUp( KeyEvent event );
-	void mouseDown( MouseEvent event );
-	void mouseDrag( MouseEvent event );
-	void mouseWheel( MouseEvent event );
-	void mouseMove( MouseEvent event );
+//	void mouseDown( MouseEvent event );
+//	void mouseDrag( MouseEvent event );
+//	void mouseWheel( MouseEvent event );
+//	void mouseMove( MouseEvent event );
 	void draw();
 	void resize( ResizeEvent event);
 	
 	Map map;
 	Vec2f pMouse;
+	
+	std::map<uint32_t, Vec2f> prevTouches;
 };
+
+void modestmapsciApp::prepareSettings(Settings *settings) {
+	settings->enableMultiTouch(true);
+}
 
 void modestmapsciApp::setup()
 {
@@ -61,9 +68,9 @@ void modestmapsciApp::keyUp( KeyEvent event)
 	}
 }
 
-void modestmapsciApp::mouseMove( MouseEvent event )
+/*void modestmapsciApp::mouseMove( MouseEvent event )
 {
-	cout << map.pointLocation(Vec2d(event.getX(), event.getY())) << endl;
+	//cout << map.pointLocation(Vec2d(event.getX(), event.getY())) << endl;
 }
 
 void modestmapsciApp::mouseDown( MouseEvent event )
@@ -90,25 +97,63 @@ void modestmapsciApp::mouseWheel( MouseEvent event )
 			map.scaleBy(delta > 0 ? 1.05 : 1.0/1.05, pos.x, pos.y);
 		}
 	}
-}
+}*/
 
 void modestmapsciApp::draw()
 {
-	gl::clear( Color( 0.1f, 0.1f, 0.1f ) );
+	gl::clear( Color( 0.0f, 0.0f, 0.0f ) );
 	map.draw();
+	gl::color( Color( 1.0f, 1.0f, 1.0f ) );
+	std::vector<TouchEvent::Touch> touches = getActiveTouches();
+	for (int i = 0; i < touches.size(); i++) {
+		gl::drawStrokedCircle( touches[i].getPos(), 20 );
+	}
 }
+
+
 
 void modestmapsciApp::touchesBegan( TouchEvent event )
 {
-	cout << event << endl;
+	for (int i = 0; i < event.getTouches().size(); i++) {
+		prevTouches[event.getTouches()[i].getId()] = event.getTouches()[i].getPos();
+	}
 }
+	
 void modestmapsciApp::touchesMoved( TouchEvent event )
 {
-	cout << event << endl;
+	std::vector<TouchEvent::Touch> touches = getActiveTouches();
+	if (touches.size() == 1) {
+		Vec2f prevTouch = prevTouches[touches[0].getId()];
+		map.panBy(touches[0].getX() - prevTouch.x, touches[0].getY() - prevTouch.y);
+		prevTouches[touches[0].getId()] = touches[0].getPos();
+	}
+	if (touches.size() == 2) {
+		Vec2f p0 = touches[0].getPos();
+		Vec2f p1 = touches[1].getPos();
+		cout << p0 << " " << p1 << endl;
+		double startDist = p0.distance(p1);
+		Vec2f startCenter = p0.lerp(0.5, p1);
+		double startRot = atan2(p1.y-p0.y,p1.x-p0.x);
+		Vec2f p2 = prevTouches[touches[0].getId()];
+		Vec2f p3 = prevTouches[touches[1].getId()];
+		cout << p2 << " " << p3 << endl;
+		double endDist = p2.distance(p3);
+		Vec2f endCenter = p2.lerp(0.5, p3);
+		double endRot = atan2(p3.y-p2.y,p3.x-p2.x);
+		//////
+		double sc = startDist / endDist;
+		double r = startRot - endRot;
+		map.panBy(startCenter.x-endCenter.x,startCenter.y-endCenter.y);
+		map.scaleBy(sc, endCenter.x, endCenter.y);
+		map.rotateBy(r, endCenter.x, endCenter.y); 
+		//////
+		prevTouches[touches[0].getId()] = p0;
+		prevTouches[touches[1].getId()] = p1;
+	}
 }
 void modestmapsciApp::touchesEnded( TouchEvent event )
 {
-	cout << event << endl;
+
 }
 	
 void modestmapsciApp::resize( ResizeEvent event )
